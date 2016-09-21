1adb2bc67dd45c152a138c8b453bfcfb9d36bdd5
Jacob Zimmerman <jczimm@jczimm.com>
2016-09-20 20:51:12 -0400

    Main.java: fix printing, variable names, finish printing

--- Saddle Points/src/Main.java
+++ Saddle Points/src/Main.java
@@ -1,16 +1,27 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// use an array filled with random points to test the algorithm
-		int[][] randPoints = new int[5][5];
+		int[][] randVals = new int[5][5];
 		Random rand = new Random();
-		for (int i = 0; i < randPoints.length - 1; i++)
-			for (int j = 0; j < randPoints[i].length - 1; j++)
-				randPoints[i][j] = rand.nextInt(10);
+		for (int i = 0; i < randVals.length; i++) {
+			for (int j = 0; j < randVals[i].length; j++)
+				System.out.print((randVals[i][j] = rand.nextInt(10)) + "  ");
+			System.out.println();
+		}
 		
-		SaddlePoints.findPoints(randPoints);
+		ArrayList<Integer[]> saddlePoints = SaddlePoints.findPoints(randVals);
+		if (saddlePoints.size() == 0) System.out.println("No saddle points");
+		else {
+			Integer[] point = new Integer[2];
+			for (int i = 0; i < saddlePoints.size(); i++) {
+				point = saddlePoints.get(i);
+				System.out.println(point[0] + ", " + point[1]);
+			}
+		}
 		
 	}
 	
