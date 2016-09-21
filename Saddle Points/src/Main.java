81cea081a458cda1e552f7e521c3f1793a0c7851
Jacob Zimmerman <jczimm.alias@gmail.com>
2016-09-20 20:22:52 -0400

    Main.java: created program entry point

--- Saddle Points/src/Main.java
+++ Saddle Points/src/Main.java
@@ -1,4 +1,17 @@
+import java.util.Random;
 
 public class Main {
 
+	public static void main(String[] args) {
+		// use an array filled with random points to test the algorithm
+		int[][] randPoints = new int[5][5];
+		Random rand = new Random();
+		for (int i = 0; i < randPoints.length - 1; i++)
+			for (int j = 0; j < randPoints[i].length - 1; j++)
+				randPoints[i][j] = rand.nextInt(10);
+		
+		SaddlePoints.findPoints(randPoints);
+		
+	}
+	
 }
