3a5472a9a9b80f58bcc91c42fa05cd90a605b737
John Wang <johnwang1218@gmail.com>
2016-09-22 20:51:41 -0400

    algorithms should work

--- Saddle Points/src/SaddlePoints.java
+++ Saddle Points/src/SaddlePoints.java
@@ -4,12 +4,13 @@ public class SaddlePoints {
 
 	public static ArrayList<Integer[]> findPoints(int[][] vals) {
 		ArrayList<Integer[]> saddlePoints = new ArrayList<Integer[]>();
-		
-		int val;
+		int row;
+		int col;
 		for (int i = 0; i < vals.length; i++) {
 			for (int j = 0; j < vals[i].length; j++) {
-				val = vals[i][j];
-				if (isSaddlePointByRow(val) && isSaddlePointByCol(val)) {
+				row = i;
+				col = j;
+				if (isSaddlePointByRow(row, col, vals) && isSaddlePointByCol(row, col, vals)) {
 					Integer[] point = { i, j };
 					saddlePoints.add(point);
 				}
@@ -19,14 +20,32 @@ public class SaddlePoints {
 		return saddlePoints;
 	}
 
-	private static boolean isSaddlePointByRow(int point) {
+	private static boolean isSaddlePointByRow(int row, int col, int[][] vals) {
 		// TODO: return true only if it's greater than or equal to every other value in its row
-		return true;
+		int rowmax = 0;
+		for (int j = 0; j < vals.length; j++) {
+			if (vals[row][j]>rowmax) {
+					rowmax = vals[row][j];
+			}
+		}
+		if (vals [row][col] >= rowmax) {
+			return true;
+		}
+		else return false;
 	}
 	
-	private static boolean isSaddlePointByCol(int point) {
+	private static boolean isSaddlePointByCol(int row, int col, int[][] vals) {
 		// TODO: return true only if it's less than or equal to every other value in its column
-		return true;
+		int colmin = 9;
+		for (int i = 0; i < vals.length; i++) {
+			if (vals[i][col]<colmin) {
+					colmin = vals[i][col];
+			}
+		}
+		if (vals [row][col] <= colmin) {
+			return true;
+		}
+		else return false;
 	}
 
 }
